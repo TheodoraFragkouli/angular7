c3057a8a872ae49d9f9e91847c1896828410e64f
Rou <tfragkouli@gmail.com>
2018-11-30 20:02:56 +0200

    section 2

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-servers]',
+  // template: `
+  //            <app-server></app-server>
+  //            <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Test Rou';
+  serverCreated=false;
+
+  constructor() {
+    setTimeout( () => {this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreated= true;
+    this.serverCreationStatus = 'Server was created. Server name is ' + this.serverName;
+  }
+
+  onUpdateServerName($event: any) {
+    console.log($event);
+    this.serverName = <HTMLInputElement>event.target.value;
+  }
+}
