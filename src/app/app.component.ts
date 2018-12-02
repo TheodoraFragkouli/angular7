c57e825d1d6a378464a98a1a4bbda382af07caad
TF <tfragkouli>
2018-12-03 00:08:06 +0200

    section3 : recipe app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-  // styles: [`
-  //   h3 {
-  //     color: dodgerblue;
-  //   }
-  // `]
 })
 export class AppComponent {
   title = 'dream-app';
-
-  showSecret = false;
-  log = []
-
-  onToggleDisplay() {
-    this.showSecret = !this.showSecret;
-    // this.log.push(this.log.length+1);
-    this.log.push(new Date());
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,11 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
