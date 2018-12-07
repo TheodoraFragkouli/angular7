846388840fd9cefa1be09680940ed4e38cbd91a2
Theodora Fragkouli <theodora.fragkouli@iriworldwide.com>
2018-12-07 17:51:22 +0200

    section3: adding ingredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from "../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+  new Ingredient("mashrooms", 10),
+  new Ingredient("onions", 1)
+  ];
 
   constructor() { }
 
