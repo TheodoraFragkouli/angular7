e709568e391581f6b0a2de28508daa7c9be7b491
Theodora Fragkouli <theodora.fragkouli@iriworldwide.com>
2019-01-09 00:04:47 +0200

    section10: shopping list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,21 +1,34 @@
 import {Recipe} from "./recipe.model";
-import {EventEmitter} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
+  constructor(private slService: ShoppingListService) {}
+
   private recipes: Recipe[] = [
     new Recipe('Mashroom velvet soup recipe',
       'Mashroom velvet soup',
-      'https://www.argiro.gr/wp-content/uploads/2016/03/manitarosoupa-beloute-me-kastana.jpg'),
+      'https://www.argiro.gr/wp-content/uploads/2016/03/manitarosoupa-beloute-me-kastana.jpg',
+      [new Ingredient('mushrooms', 10),
+        new Ingredient('onions', 2)]),
 
     new Recipe('Turkey recipe',
       'turkey filled',
-      'https://www.sintayes.gr/wp-content/uploads/2014/11/turkey_0-638x350.jpg')
+      'https://www.sintayes.gr/wp-content/uploads/2014/11/turkey_0-638x350.jpg',
+      [new Ingredient('turkey', 1),
+        new Ingredient('potatoes', 10)])
   ];
 
   getRecipes() {
     return this.recipes.slice();//return copy of the array
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,24 @@
-export class ShoppingListService {
+import {Ingredient} from "../shared/ingredient.model";
+import {EventEmitter} from "@angular/core";
 
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient("mushrooms", 10),
+    new Ingredient("onions", 1)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);//spread
+    this.ingredientsChanged.emit(this.ingredients);
+  }
 }
